daf7d1415b05df6306c987273481abbac33286c9
mmxd12 <73402119+mmxd12@users.noreply.github.com>
2026-08-07 17:19:05 +0800

    feat: 添加 PM2 配置文件
    feat: 添加 PM2 配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'warframe-info-api',
+    script: './bin/www',
+    instances: 1,
+    exec_mode: 'fork',
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 3000,
+    watch: false,
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+};
